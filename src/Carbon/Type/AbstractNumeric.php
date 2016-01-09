d62e0ce3d835bedd80ec2a9a843fb3021375ecb6
marceloeverson <ewan.rpg@hotmail.com>
2016-01-09 21:59:54 -0200

    remove operations of AbstractNumeric

--- src/Carbon/Type/AbstractNumeric.php
+++ src/Carbon/Type/AbstractNumeric.php
@@ -30,52 +30,6 @@ abstract class AbstractNumeric extends AbstractPrototype implements ParsableInte
     {
      return $this->get($name);
     }
-    public function add(...$values)
-    {
-        if($this->validate($values)) {
-            $this->value += call_user_func($this->converter, array_sum($values));
-
-        }
-        return $this;
-    }
-
-    public function mul(...$values)
-    {
-        if($this->validate($values)) {
-            $this->value *= call_user_func($this->converter, array_product($values));
-
-        }
-        return $this;
-    }
-
-    public function sub(...$values)
-    {
-        if($this->validate($values)) {
-            array_walk($values, function ($num) {
-                $this->value -= call_user_func($this->converter, $num);
-            });
-
-        }
-        return $this;
-
-    }
-
-    public function div(...$values)
-    {
-        if($this->validate($values)) {
-            array_walk($values, function ($num, $index) {
-                if (intval($num) != 0) {
-                    $this->value /= call_user_func($this->converter, $num);
-                    $this->value  = call_user_func($this->converter, $this->value);
-                    return $this->value;
-                }
-                throw new \ErrorException("Division by zero.Value in index $index not's accept");
-            });
-
-        }
-        return $this;
-
-    }
     public function valueOf()
     {
         return $this->value;
